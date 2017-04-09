4ff2e30fcd51118c867d0c7a4be6c4b4c04aa3ec
xabertum <mohatar@gmail.com>
2017-04-09 11:53:01 +0200

    first commit

--- public_html/ejercicio1.js
+++ public_html/ejercicio1.js
@@ -5,3 +5,24 @@
  */
 
 
+document.title = "Hola";
+
+
+function  inicializar()
+{
+    document.getElementById("crear-ventana").onclick = crearNueva;
+    document.getElementById("cerrar-ventana").onclick = cerrarNueva;
+}
+var nuevaVentana;
+function  crearNueva()
+{
+    nuevaVentana = window.open("http://www.google.es", "", "height=400,width=800");
+}
+function  cerrarNueva()
+{
+    if (nuevaVentana)
+    {
+        nuevaVentana.close();
+        nuevaVentana = null;
+    }
+}
