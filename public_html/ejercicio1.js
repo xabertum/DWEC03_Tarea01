6260ac962c6112cf8bdd2bf1ae1e559d0876158f
xabertum <mohatar@gmail.com>
2017-04-10 12:06:55 +0200

    minor changes

--- public_html/ejercicio1.js
+++ public_html/ejercicio1.js
@@ -26,3 +26,10 @@ function  cerrarNueva()
         nuevaVentana = null;
     }
 }
+
+
+document.write(Math.cos(3) + "<br />");
+document.write(Math.asin(0) + "<br />"); 
+document.write(Math.max(0,150,30,20,38) + "<br />"); 
+document.write(Math.pow(7,2) + "<br />"); 
+document.write(Math.round(0.49) + "<br />");
