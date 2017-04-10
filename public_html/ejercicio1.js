58d4c587ca5af02ff34c0ef06360e2f4f46f6550
xabertum <mohatar@gmail.com>
2017-04-10 22:31:39 +0200

    minor changes

--- public_html/ejercicio1.js
+++ public_html/ejercicio1.js
@@ -8,32 +8,4 @@
 document.title = "Hola";
 
 
-function  inicializar()
-{
-    document.getElementById("crear-ventana").onclick = crearNueva;
-    document.getElementById("cerrar-ventana").onclick = cerrarNueva;
-}
-var nuevaVentana;
-function  crearNueva()
-{
-    nuevaVentana = window.open("http://www.google.es", "", "height=400,width=800");
-}
-function  cerrarNueva()
-{
-    if (nuevaVentana)
-    {
-        nuevaVentana.close();
-        nuevaVentana = null;
-    }
-}
-
-
-var num = new Number(54.1234);
-document.write(num.toPrecision(3)+ "<br/>");
-document.write(num.toFixed(1) + "<br/>");
-document.write(num.toString(2) + "<br/>");
-document.write(num.toString(8) + "<br/>");
-document.write(num.toString(16) + "<br/>");
-document.write(Number.MIN_VALUE);
-document.write(Number.MAX_VALUE);
 
