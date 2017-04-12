36859676e574f6817124c293b1c913831aa513f3
xabertum <mohatar@gmail.com>
2017-04-12 12:37:22 +0200

    minor changes

--- public_html/ejercicio1.js
+++ public_html/ejercicio1.js
@@ -9,23 +9,37 @@ document.title = "Hola";
 
 NUMMAXELEMENTOS = 0;
 
-
 function create(numMaxElementos) {
 
     NUMMAXELEMENTOS = numMaxElementos;
-    var lista = new Array();
+    var lista = new Array(numMaxElementos);
     return lista;
-
 }
 
 
-function isEmpty(lista) {
+function  isEmpty(lista) {
 
     return (lista.length == 0);
-
 }
 
+function  isFull (lista) {
+    
+    return (lista.length == NUMMAXELEMENTOS);
+    
+}
 
+function size (lista) {
+    
+    return lista.length;
+    
+}
+
+function add (lista, elemento) {
+    
+    
+    
+    
+} 
 
 
 
